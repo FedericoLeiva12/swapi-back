d16714cefd7d59df27a19531be1b13c48d391fa2
InvBoy <49959270+FedericoLeiva12@users.noreply.github.com>
2023-09-29 19:17:01 +0000

    fix cors error

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use((_, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+  });
   await app.listen(3001);
 }
 bootstrap();
